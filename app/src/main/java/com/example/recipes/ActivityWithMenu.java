ebfad94196609c5113dbd448f70b885e10c39bbe
nahshonc <nahshonnn123@gmail.com>
2025-03-03 14:23:33 +0200

    נשאר קצת

--- app/src/main/java/com/example/recipes/ActivityWithMenu.java
+++ app/src/main/java/com/example/recipes/ActivityWithMenu.java
@@ -1,10 +1,13 @@
 package com.example.recipes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -25,5 +28,15 @@ public class ActivityWithMenu extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.ilovecooking, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        Intent get = new Intent(this, RecipesByTypeActivity.class);
+        String title = item.getTitle().toString();
+        get.putExtra("title",title);
+        startActivity(get);
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/recipes/RecipesByTypeActivity.java
@@ -0,0 +1,40 @@
+package com.example.recipes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RecipesByTypeActivity extends ActivityWithMenu {
+     TextView tvTitle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recipes_by_type);
+        Intent gotten = this.getIntent();
+        String title;
+        initViews();
+        Bundle extras = gotten.getExtras();
+        if(extras == null) {
+            title = null;
+        } else {
+            title= extras.getString("title");
+        }
+
+
+        tvTitle.setText(title);
+
+    }
+
+    private void initViews() {
+        tvTitle = findViewById(R.id.titleView);
+    }
+
+
+}
