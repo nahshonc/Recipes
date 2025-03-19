9fb95fcbead84809c85e7ce99a330140d154a80c
nahshonc <nahshonnn123@gmail.com>
2025-03-19 14:26:20 +0200

    נשאר קצת

--- app/src/main/java/com/example/recipes/RecipesByTypeActivity.java
+++ app/src/main/java/com/example/recipes/RecipesByTypeActivity.java
@@ -12,6 +12,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RecipesByTypeActivity extends ActivityWithMenu {
      TextView tvTitle;
+     private String name;
+     private String time;
+     private int cost;
+
+    public RecipesByTypeActivity(String name, String time, int cost) {
+        this.time = time;
+        this.name = name;
+        this.cost = cost;
+    }
+
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
